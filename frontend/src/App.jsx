33675766c07fb05f67bcc102510ca75b7c906243
Camila Franco <camilafranco512@gmail.com>
2024-10-13 18:51:41 -0300

    not found page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,14 @@ import { Routes, Route } from 'react-router-dom';
 import { RegisterUser } from './pages/RegisterUser';
 import { AdminRoutes } from './admin/AdminRoutes'
 import { Home } from './pages/Home';
+import NotFound from './pages/errors/notFound';
 
 function App() {
 
   return (
     <>
     <Routes>
+        <Route path="*" element={<NotFound/>}></Route>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/register" element={<RegisterUser/>}></Route>
         <Route path="/admin/*" element={<AdminRoutes/>}></Route>
