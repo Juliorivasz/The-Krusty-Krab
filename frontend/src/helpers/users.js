93a141a65b9d248950d6f8dd164c4fd83cc8f82c
Juliorivas <julioandresrivas@gmail.com>
2024-10-17 22:09:03 -0300

    info user

---
+++ frontend/src/helpers/users.js
@@ -0,0 +1,10 @@
+export const user = {
+    name: 'Julio Rivas',
+    email: 'julioandresrivas@gmail.com',
+    isAdmin: true,
+    phone: '2615752890',
+    address: [
+        'carola lorenzini 300, godoy cruz'
+    ]
+
+}
