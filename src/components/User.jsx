bedf58eae6f3a29f21c011b8106e50945daffb3b
GiovanniGiannoni <giovannigiannoni13@gmail.com>
2024-10-23 18:42:06 -0300

    subo notificaciones

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import { useState } from "react";
 import { ProfileDropDown } from "../modal/ProfileDropDown.jsx"; // Importa el menú desplegable
 
-export const User = ({handleShowEmptyCart}) => {
+export const User = ({handleShowEmptyCart,showNotifications, setShowNotifications}) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false); // Manejo del estado del menú
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen); // Alterna el estado del menú
@@ -15,9 +15,13 @@ export const User = ({handleShowEmptyCart}) => {
     setIsMenuOpen(false); // Cierra el menú
   };
 
+  const toggleNotifications = () => {
+    setShowNotifications(!showNotifications);
+  };
+
   return (
     <div className="container-notify-cart-profile">
-      <div className="notification">
+      <div className="notification" onClick={toggleNotifications}>
         <img src={notification} alt="Notificación" />
       </div>
       <div className="cart" onClick={handleShowEmptyCart}>
